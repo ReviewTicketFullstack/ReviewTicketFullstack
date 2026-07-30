484906969dd87c55ad1da5e9f72ec20b49eaee08
byu-rin <simon3397@gmail.com>
2026-07-30 13:53:27 +0900

    [design] 바텀 내비 선 삭제

--- src/shared/layout/CustomerLayout/BottomNavigation/BottomNavigation.tsx
+++ src/shared/layout/CustomerLayout/BottomNavigation/BottomNavigation.tsx
@@ -1,20 +1,20 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 export function BottomNavigation() {
   return (
-    <nav className="sticky bottom-0 flex h-16 border-t bg-white">
-      <NavLink 
-        to="/home" 
-        className={({ isActive }) => 
-          `flex-1 flex items-center justify-center text-sm ${isActive ? 'text-blue-600 font-bold' : 'text-gray-500'}`
+    <nav className="sticky bottom-0 flex h-16 bg-white">
+      <NavLink
+        to="/home"
+        className={({ isActive }) =>
+          `flex-1 flex items-center justify-center text-sm ${isActive ? "text-blue-600 font-bold" : "text-gray-500"}`
         }
       >
         홈
       </NavLink>
-      <NavLink 
-        to="/order-history" 
-        className={({ isActive }) => 
-          `flex-1 flex items-center justify-center text-sm ${isActive ? 'text-blue-600 font-bold' : 'text-gray-500'}`
+      <NavLink
+        to="/order-history"
+        className={({ isActive }) =>
+          `flex-1 flex items-center justify-center text-sm ${isActive ? "text-blue-600 font-bold" : "text-gray-500"}`
         }
       >
         주문내역
